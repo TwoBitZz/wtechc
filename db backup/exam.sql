97ce777d050f235adb963f299326c988cef871e6
sabeel muttil <sabeelmuttil@gmail.com>
2017-11-01 13:44:55 +0530

    hi

--- db backup/exam.sql
+++ db backup/exam.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 24, 2017 at 07:11 PM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: Nov 01, 2017 at 09:12 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `exam`
 --
-CREATE DATABASE IF NOT EXISTS `exam` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `exam`;
 
 -- --------------------------------------------------------
 
@@ -28,81 +28,19 @@ USE `exam`;
 -- Table structure for table `hak`
 --
 
-DROP TABLE IF EXISTS `hak`;
-CREATE TABLE IF NOT EXISTS `hak` (
+CREATE TABLE `hak` (
   `hid` int(10) NOT NULL,
   `hname` varchar(100) NOT NULL,
-  `hpass` varchar(100) NOT NULL,
-  PRIMARY KEY (`hid`)
+  `hpass` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Truncate table before insert `hak`
---
-
-TRUNCATE TABLE `hak`;
---
--- Dumping data for table `hak`
---
-
-INSERT DELAYED IGNORE INTO `hak` (`hid`, `hname`, `hpass`) VALUES
-(1, 'use', 'pass');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hak0`
---
-
-DROP TABLE IF EXISTS `hak0`;
-CREATE TABLE IF NOT EXISTS `hak0` (
-  `h0id` int(10) NOT NULL,
-  `onlypass` varchar(100) NOT NULL,
-  PRIMARY KEY (`h0id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Truncate table before insert `hak0`
---
-
-TRUNCATE TABLE `hak0`;
---
--- Dumping data for table `hak0`
---
-
-INSERT DELAYED IGNORE INTO `hak0` (`h0id`, `onlypass`) VALUES
-(1, '2017');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reslt`
---
-
-DROP TABLE IF EXISTS `reslt`;
-CREATE TABLE IF NOT EXISTS `reslt` (
-  `userId` int(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `s1` int(100) NOT NULL,
-  `s2` int(100) NOT NULL,
-  `s3` int(100) NOT NULL,
-  `s4` int(100) NOT NULL,
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Truncate table before insert `reslt`
---
-
-TRUNCATE TABLE `reslt`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `stg1`
 --
 
-DROP TABLE IF EXISTS `stg1`;
-CREATE TABLE IF NOT EXISTS `stg1` (
+CREATE TABLE `stg1` (
   `id` int(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `first` int(100) NOT NULL,
@@ -115,21 +53,8 @@ CREATE TABLE IF NOT EXISTS `stg1` (
   `eight` int(100) NOT NULL,
   `nine` int(100) NOT NULL,
   `ten` int(100) NOT NULL,
-  `tot` int(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  `tot` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Truncate table before insert `stg1`
---
-
-TRUNCATE TABLE `stg1`;
---
--- Dumping data for table `stg1`
---
-
-INSERT DELAYED IGNORE INTO `stg1` (`id`, `username`, `first`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`, `tot`) VALUES
-(0, 'sabeelmuttil', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -137,27 +62,57 @@ INSERT DELAYED IGNORE INTO `stg1` (`id`, `username`, `first`, `two`, `three`, `f
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `userId` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `userId` int(100) NOT NULL,
   `username` varchar(100) NOT NULL,
   `useremail` varchar(100) NOT NULL,
-  `userpass` varchar(100) NOT NULL,
-  PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `userpass` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Truncate table before insert `users`
---
-
-TRUNCATE TABLE `users`;
 --
 -- Dumping data for table `users`
 --
 
-INSERT DELAYED IGNORE INTO `users` (`userId`, `username`, `useremail`, `userpass`) VALUES
+INSERT INTO `users` (`userId`, `username`, `useremail`, `userpass`) VALUES
 (1, 'sabeelmuttil', 'sabeelmuttil@gmail.com', '1f67913d9ee5dbc9986e9684a373fcdf1f65a73ddaa02a88960e8f88d809843c'),
 (2, 'MUHAMMAD ANAS', 'saffa@gmail.com', '15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hak`
+--
+ALTER TABLE `hak`
+  ADD PRIMARY KEY (`hid`);
+
+--
+-- Indexes for table `stg1`
+--
+ALTER TABLE `stg1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stg1`
+--
+ALTER TABLE `stg1`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
